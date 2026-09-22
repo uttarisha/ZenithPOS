5fe29900c7ec716c9fcbd480970239c7bf800dce
Uttarisha <uttarishabhattacharya26@gmail.com>
2026-09-23 02:13:03 +0530

    API URL change

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const BASE_URL = "http://localhost:8081"
+export const BASE_URL = "https://zenithpos-backend.onrender.com/"
 
 const api = axios.create({
   baseURL: `${BASE_URL}/api`,
